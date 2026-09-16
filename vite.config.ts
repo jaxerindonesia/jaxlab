352a97b878d92adb70aefd2e3f6790442e851bb3
suryadharmabakti <suryadharma.baktirm@gmail.com>
2026-09-16 10:07:51 +0700

    [SR] Vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['jaxlabofficial.id', 'www.jaxlabofficial.id'],
+  },
   server: {
     proxy: {
       '/api': {
